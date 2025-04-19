23696c44e5867b4255a9081c6ff6862b32d62ace
George Z <george.zhu@students.rsd.edu>
2025-04-18 20:22:57 -0700

    Beta v1.0.7 - Changed apps so they now go through into the loader rather than their own thing (yes this means proxy ui maybe)

--- loader.js
+++ loader.js
@@ -1,13 +1,23 @@
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", async function() {
     const urlParams = new URLSearchParams(window.location.search);
     const name = urlParams.get('name');
     const link = urlParams.get('link');
+    const type = urlParams.get('type');
+    
+    const backLink = type === "app" ? "apps.html" : "games.html";
+    document.getElementById('back-link').href = backLink;
 
-    if (name) {
+    if (name && document.getElementById('title')) {
         document.getElementById('title').textContent = name;
     }
 
-    if (link) {
-        document.getElementById('game-frame').src = link;
+    if (link && document.getElementById('main-frame')) {
+        if (type === "app") {
+            const uvUrl = __uv$config.prefix + __uv$config.encodeUrl(link);
+            document.getElementById('main-frame').src = uvUrl;
+            document.getElementById('uv-form').style.display = 'none';
+        } else {
+            document.getElementById('main-frame').src = link;
+        }
     }
-};
+});
